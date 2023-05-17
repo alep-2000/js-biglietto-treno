9064262f2d459c531f35dfd3eeac5a90faf8252b
alep-2000 <alessiopiras056@gmail.com>
2023-05-17 16:27:42 +0200

    [FEAT STYLE]Exercise finisched

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+let kilometres = parseInt(prompt('Quanti km vuoi percorrere?'));
+
+let age = parseInt(prompt('Quanti anni hai?'));
+
+let prizetokm = 0.21;
+
+let prizefinal= prizetokm * kilometres;
+
+if (age < 18) {
+    prizefinal =(prizetokm * kilometres - 20/100);
+}else if(age > 65){
+    prizefinal =(prizetokm * kilometres - 40/100);
+}else{
+    prizefinal =(prizetokm * kilometres);
+}
+
+document.getElementById('ticket').innerHTML=  ` Paghi ${prizefinal}€`;
