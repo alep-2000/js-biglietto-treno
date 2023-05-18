38c1dc6eb7044ef3310129522d5f8e5851ecd87f
alep-2000 <alessiopiras056@gmail.com>
2023-05-18 10:05:30 +0200

    [FEAT STYLE]Add tofixed

--- JS/script.js
+++ JS/script.js
@@ -6,12 +6,12 @@ let prizetokm = 0.21;
 
 let prizefinal= prizetokm * kilometres;
 
-if (age < 18) {
-    prizefinal =(prizetokm * kilometres - 20/100);
+if (age < 18){
+    prizefinal =(prizetokm * kilometres - 20/100).toFixed(2);
 }else if(age > 65){
-    prizefinal =(prizetokm * kilometres - 40/100);
+    prizefinal =(prizetokm * kilometres - 40/100).toFixed(2);
 }else{
-    prizefinal =(prizetokm * kilometres);
+    prizefinal =(prizetokm * kilometres).toFixed(2);
 }
 
 document.getElementById('ticket').innerHTML=  ` Paghi ${prizefinal}€`;
